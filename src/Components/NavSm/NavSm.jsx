4fe76d775577f6dd996ea001ab155938674abc87
Abdul Saboor <70238888+AbdulSaboor100@users.noreply.github.com>
2023-02-24 07:43:41 -0800

    Completed heroheader and navbar

--- src/Components/NavSm/NavSm.jsx
+++ src/Components/NavSm/NavSm.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const Navbar = () => {
+const NavSm = () => {
   const [bgColor, setBgColor] = useState(false);
   const styleHandler = () => {
     if (window.scrollY >= 80) {
@@ -16,17 +16,24 @@ const Navbar = () => {
 
   return (
     <div
-      className={`w-full fixed top-0 sm:hidden ${
+      className={`w-full fixed sm:block top-0  ${
         bgColor ? onScrollStyling : ""
       }`}
     >
       <div
-        className={`w-full h-[66px] flex justify-between items-center max-w-m1170 m-auto p-4 `}
+        className={`w-full h-full flex justify-between items-center  p-4 relative`}
       >
         <div>
           <p className={`text-black font-w7 text-h5 capitalize`}>Reanan.</p>
         </div>
         <div className={`flex gap-7 items-center`}>
+          <i className="bx bx-menu text-h4 text-main_primary cursor-pointer"></i>
+        </div>
+        <div
+          className={
+            "w-full h-full bg-black p-4 flex flex-col gap-4 absolute left-0 right-0 bottom-0 top-16"
+          }
+        >
           <p className={`text-main_primary text-small font-w5 cursor-pointer`}>
             Home
           </p>
@@ -46,4 +53,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default NavSm;
